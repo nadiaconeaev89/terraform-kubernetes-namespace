1283e65736301954fa6a34e5327df9c220b86b52
nadiaconeaev89 <nadejda.savin@gmail.com>
2023-11-22 18:41:33 -0600

    Added size

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ resource "kubernetes_namespace" "example" {
   }
 }
 
+resource "kubernetes_resource_quota" "pod-limit" {
+  metadata {
+    name = "pod-limit"
+  }
+  spec {
+    hard = {
+      pods = 1000
+    }
+    scopes = ["BestEffort"]
+  }
+}
